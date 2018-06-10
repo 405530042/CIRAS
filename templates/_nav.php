d3b980f8c3482df1edb61b6b96de210598cd27fb
Fichufish <39399950+Fichufish@users.noreply.github.com>
2018-06-11 03:44:12 +0800

    6/11 update all files

--- templates/_nav.php
+++ templates/_nav.php
@@ -11,7 +11,13 @@
     	<div id="head_txt">
 				<div id="nav">
 					<ul>
-						<li><a href="http://agei.ccu.edu.tw/news1.php" target="_blank">最新消息</a></li>
+						<li><a href="">最新消息</a>
+							<ul>
+								<li><a href="article.php">活動公告</a></li>
+								<li><a href="eventcollect.php">時事蒐集</a></li>
+								<li><a href="newscollect.php">新聞快訊</a></li>
+							</ul>
+						</li>
 						<li><a href="about.php">關於中心</a></li>
 						<li><a href="iaa.php">產學聯盟</a></li>
 						<li><a href="training.php">人才培力</a></li>
