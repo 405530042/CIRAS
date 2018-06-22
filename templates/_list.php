c9d36405ab056bc317397edac6c0df60761b71da
Fichufish <39399950+Fichufish@users.noreply.github.com>
2018-06-22 15:18:18 +0800

    6/22 evening add

--- templates/_list.php
+++ templates/_list.php
@@ -1,36 +1,18 @@
 	<ul>
-		<li><a href="establish.php">成立宗旨</a></li>
-		<li><div class="expander exclose"></div><a href="struct.php">總體營運說明</a>
-		  <ul>
-			<li><a href="status.php">推動目標及整體現況</a></li>
-			<li><div class="expander exclose"></div><a href="past5y.php">近五年成果</a>
-			  <ul>
-				<li><a href="slearning.php">服務學習成果</a></li>
-				<li><a href="mediareport.php">媒體報導</a></li>
-			  </ul>
+		<li><a href="about.php">關於中心</a>
+		<ul>
+			<li><a href="establish.php">成立宗旨</a></li>
+			<li><a>中心介紹</a></li>
+			<li><a>大事紀</a></li>
+			<li><a>發展歷程</a></li>
+			<li><a href="struct.php">總體營運</a>
+		 	<ul>
+				<li><a>目標設定</a></li>
+				<li><a>經營策略</a></li>
+				<li><a>成果展示</a></li>
+				<li><a>預期貢獻</a></li>
+		  	</ul>
 			</li>
-			<li><a href="centermember.php">中心成員</a></li>
-			<li><a href="swot.php">自我評估與SWOT分析</a></li>
-			<li><a href="contribution.php">對社會及產業的貢獻</a></li>
-		  </ul>
+		</ul>
 		</li>
-		<li><a href="goal.php">目標</a></li>
-		<li><div class="expander exclose"></div><a href="stradgey.php">策略</a>
-		  <ul>
-			<li><a href="scwibc.php">強化與國際標竿中心合作</a></li>
-			<li><div class="expander exclose"></div><a href="ards.php">學術研究發展策略</a>
-			  <ul>
-				<li><a href="/CIRAS/social.php">社會轉型研究群</a></li>
-				<li><a>高齡生理、心理、認知與運動健康研究群</a></li>
-				<li><a>生活科技與照護科技研究群</a></li>
-			  </ul>
-			</li>
-			<li><a>協助社會共創發展規劃</a></li>
-			<li><a>協助產業發展規劃</a></li>
-			<li><a>延攬優秀人才規劃</a></li>
-			<li><a>培育高階研發人才規劃</a></li>
-			<li><a>外部資源連結規劃</a></li>
-		  </ul>
-		</li>
-		<li><a  href="susdev.php">永續經營策略</a></li>
 	</ul>
--- templates/_list_training.php
+++ templates/_list_training.php
@@ -1,8 +1,8 @@
 	<ul>
-		<li><a href="training.php">人才培育規劃</a></li>
+		<li><a href="training.php">人才培育</a></li>
 		  <ul>
-		  	<li><a>推動高齡人力再運用</a></li>
-			<li><a>培育高齡產業與相關服務人才</a></li>
+		  	<li><a href="training.php">推動高齡人力再運用</a></li>
+			<li><a href="csit.php">培育高齡產業與相關服務人才</a></li>
 		  </ul>
 		</li>
 	</ul>
