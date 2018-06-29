5a2d4893d05a27a8e994446c718aa6654f198e56
Fichufish <39399950+Fichufish@users.noreply.github.com>
2018-06-29 09:35:33 +0800

    Add files via upload

--- templates/_list.php
+++ templates/_list.php
@@ -2,12 +2,12 @@
 		<li><a href="about.php">關於中心</a>
 		<ul>
 			<li><a href="establish.php">成立宗旨</a></li>
-			<li><a>中心介紹</a></li>
+			<li><a href="intro.php">中心介紹</a></li>
 			<li><a>大事紀</a></li>
-			<li><a>發展歷程</a></li>
+			<li><a href="record.php">發展歷程</a></li>
 			<li><a href="struct.php">總體營運</a>
 		 	<ul>
-				<li><a>目標設定</a></li>
+				<li><a href="target.php">目標設定</a></li>
 				<li><a>經營策略</a></li>
 				<li><a>成果展示</a></li>
 				<li><a>預期貢獻</a></li>
--- templates/_upon.php
+++ templates/_upon.php
@@ -1,4 +1,5 @@
 <ul>
-	<li><a href="http://agei.ccu.edu.tw" target="_blank">　國立中正大學高齡研究基地　</a></li>
-	<li><a href="login.php">|　Login</a></li>
+	<li style="border-right:1px solid black ;width:190px;text-align: center;"><a href="http://agei.ccu.edu.tw" target="_blank" d>國立中正大學高齡研究基地</a></li>
+	<li style="border-right:1px solid black ; width:50px;text-align:center;"><a href="login.php">Login</a></li>
+	<li style="width: 60px;text-align:center"><a>English</a></li>
 </ul>
