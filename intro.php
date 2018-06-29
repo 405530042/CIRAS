2f8ecf67071441a083536b2ef4acd147d0c52997
Fichufish <39399950+Fichufish@users.noreply.github.com>
2018-06-29 09:36:32 +0800

    6/28

---
+++ intro.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include('templates/_head.php'); ?>
+</head>
+<body id="body">
+	<?php include('templates/_nav.php'); ?>
+	<div class="bw"></div>
+	<div id="head_photo">
+		<div id="head_img">
+		</div>
+	</div>
+	<div id="page">
+		<div id="content">
+			<table>
+			  <tr>
+				<td>
+					<div id="list">
+						<?php include('templates/_list.php'); ?>
+					</div>
+				</td>
+				<td>
+				<div id='topic'>
+					<h1>中心架構</h1>
+						<div id="constpic">
+							<img src="images/const.png">
+						</div>
+					</div>
+						<div id='topic'>
+					<h1>成員簡介</h1>
+						<div id="constpic">
+							<img src="images/center.jpg">
+						</div>
+					</div>
+						<div id='topic'>
+					<h1>研究人員</h1>
+						<div id="constpic">
+							<img src="images/researcher1.png">
+						</div>
+						</div>
+							<div id="constpic">
+							<img src="images/researcher2.png">
+						</div>
+				</td>
+			  </tr>
+			</table>
+		</div>
+	</div>
+	<div id="footer">
+<?php include("templates/_footer.php"); ?>
+	</div>
+</body>
+<script type="text/javascript">
+	/*$(function(){
+		// nav 預設顯示第一個 Tab
+		var _showTab = 0;
+		var $defaultLi = $('#nav ul li').eq(_showTab).addClass('active');
+		// 當 li 頁籤被點擊時...
+		$('#nav ul li').click(function() {
+			var $this = $(this),
+				_clickTab = $this.find('a').attr('href');
+			$this.addClass('active').siblings('.active').removeClass('active');
+			return false;
+		}).find('a').focus(function(){
+			this.blur();
+		});
+	});*/
+	$(document).ready(function(){
+		$(".expander").click(function(){
+			var $p = $(this).parent('li');
+			$p.children('ul').slideToggle();
+
+			$(this).toggleClass('exclose');
+		});
+	});
+</script>
+</html>
---
+++ target.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include('templates/_head.php'); ?>
+</head>
+<body id="body">
+	<?php include('templates/_nav.php'); ?>
+	<div class="bw"></div>
+	<div id="head_photo">
+		<div id="head_img">
+		</div>
+	</div>
+	<div id="page">
+		<div id="content">
+			<table>
+			  <tr>
+				<td>
+					<div id="list">
+						<?php include('templates/_list.php'); ?>
+					</div>
+				</td>
+				<td>
+				<div id='topic'>
+					<h1 style="color:#cf0505;">社會實踐與服務面</h1>
+						<ul>
+							<li style="list-style-image:url('images/checkicon.png');color: #616161;font-size: 1.5em;font-family: 'Microsoft JhengHei';line-height: 1.5em;margin: 2% 0;">營造超高齡社會整體友善環境</li>
+							<li style="list-style-image:url('images/checkicon.png');color: #616161;font-size: 1.5em;font-family: 'Microsoft JhengHei';line-height: 1.5em;margin: 2% 0;">研擬活躍老化之社會互助支持系統</li>
+							<li style="list-style-image:url('images/checkicon.png');color: #616161;font-size: 1.5em;font-family: 'Microsoft JhengHei';line-height: 1.5em;margin: 2% 0;">推展全人與多面向整合性社會政策</li>
+							<li style="list-style-image:url('images/checkicon.png');color: #616161;font-size: 1.5em;font-family: 'Microsoft JhengHei';line-height: 1.5em;margin: 2% 0;">協助我國社會順利轉型</li>
+						</ul>
+						</div>
+						<div id='topic'>
+					<h1 style="color:#cf0505;">高齡跨域研究面</h1>
+						<ul>
+							<li style="list-style-image:url('images/checkicon.png');color: #616161;font-size: 1.5em;font-family: 'Microsoft JhengHei';line-height: 1.5em;margin: 2% 0;">研究發展<br>&nbsp;&nbsp;-&nbsp;&nbsp;深入研析超高齡社會變遷趨勢</li>
+							<li style="list-style-image:url('images/checkicon.png');color: #616161;font-size: 1.5em;font-family: 'Microsoft JhengHei';line-height: 1.5em;margin: 2% 0;">社會實踐<br>&nbsp;&nbsp;-&nbsp;&nbsp;開發高齡社會智慧生活多元服務</li>
+							
+						</ul>
+						</div>
+						<div id='topic'>
+					<h1 style="color:#cf0505;">產業發展面</h1>
+						<ul>
+							<li style="list-style-image:url('images/checkicon.png');color: #616161;font-size: 1.5em;font-family: 'Microsoft JhengHei';line-height: 1.5em;margin: 2% 0;">規劃高齡社會產學鏈結發展<br>&nbsp;&nbsp;-&nbsp;&nbsp;智慧生活健康照護的研究</li>
+							<li style="list-style-image:url('images/checkicon.png');color: #616161;font-size: 1.5em;font-family: 'Microsoft JhengHei';line-height: 1.5em;margin: 2% 0;">發展長壽社會健康照護產業<br>&nbsp;&nbsp;-&nbsp;&nbsp;產品與服務模式研發</li>
+							<li style="list-style-image:url('images/checkicon.png');color: #616161;font-size: 1.5em;font-family: 'Microsoft JhengHei';line-height: 1.5em;margin: 2% 0;">健康個人和健全社會促進<br>&nbsp;&nbsp;-&nbsp;&nbsp;人才訓練及教育推廣</li>
+							
+						</ul>
+						</div>
+
+				</td>
+			  </tr>
+			</table>
+		</div>
+	</div>
+	<div id="footer">
+<?php include("templates/_footer.php"); ?>
+	</div>
+</body>
+<script type="text/javascript">
+	/*$(function(){
+		// nav 預設顯示第一個 Tab
+		var _showTab = 0;
+		var $defaultLi = $('#nav ul li').eq(_showTab).addClass('active');
+		// 當 li 頁籤被點擊時...
+		$('#nav ul li').click(function() {
+			var $this = $(this),
+				_clickTab = $this.find('a').attr('href');
+			$this.addClass('active').siblings('.active').removeClass('active');
+			return false;
+		}).find('a').focus(function(){
+			this.blur();
+		});
+	});*/
+	$(document).ready(function(){
+		$(".expander").click(function(){
+			var $p = $(this).parent('li');
+			$p.children('ul').slideToggle();
+
+			$(this).toggleClass('exclose');
+		});
+	});
+</script>
+</html>
